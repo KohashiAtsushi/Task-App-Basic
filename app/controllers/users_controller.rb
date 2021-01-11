ba0295ae456f8dacb06e39b85e72f2604f0f64ca
KohashiAtsushi <melka.aquan@gmail.com>
2021-01-11 13:43:45 +0000

    add-user-show-page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,18 @@
 class UsersController < ApplicationController
   
-  def show
-    @user = User.find(params[:id])
-  end
-
+  
   def new
     @user = User.new
   end
+  
+  def show
+    @user = User.find(params[:id])
+  end
+  
+  def edit
+    @user = User.find(params[:id])
+  end
+  
   
   def create
     @user = User.new(user_params)
@@ -18,6 +24,27 @@ class UsersController < ApplicationController
       render :new
     end
   end
+  
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      flash[:success] = '新規作成に成功しました。'
+      redirect_to @user
+    else
+      render :edit
+    end
+  end
+  
+  def destroy
+    if @task.destroy
+      flash[:success] = '削除に成功しました。'
+      redirect_to user_tasks_url
+    else
+      render :user_tasks_url
+    end
+  end
+  
+  
   
   private
   
--- db/seeds.rb
+++ db/seeds.rb
@@ -3,4 +3,5 @@
 User.create!( name: "SampleUser",
               email: "sample@email.com",
               password: "password",
-              password_confirmation: "password")
+              password_confirmation: "password",
+              admin: true)
