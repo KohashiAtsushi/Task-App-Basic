9b98a0905cb03e5ebf0a767996e21c71eab72692
KohashiAtsushi <melka.aquan@gmail.com>
2021-01-11 13:02:34 +0000

    append-task-contents-to-url

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,4 +8,22 @@ module ApplicationHelper
       page_name + " | " + base_title
     end
   end
+  
+  def convert_text_to_url(text)
+    ## 引数のテキストから、URIを抜き出す。
+    ## →URL。ここの場合、リンクが欲しいので、http,https。指定するだけでそのまま指定したものを丸ごと全部抜き出せる
+    ## uniqは同じURLがある可能性があり、重複してループするのを避けるために入れている。
+    URI.extract(text, ['http','https']).uniq.each do |url|
+      ## 何回も使うので初期化
+      sub_text = ""
+      
+      ## 文字列追加、バクスラはダブルコーテーションを入れるためのエスケープシーケンス
+      ## targetはリンクの表示方法、これは新しく開いて画面に表示させる。
+      sub_text << "<a href=" << url << " target=\"_blank\">" << url << "</a>"
+      
+      ## 文字置き換え。textないのurlを全て置換。これで重複した物も変更される
+      text.gsub!(url, sub_text)
+    end
+    return text
+  end
 end
